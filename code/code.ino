87ff80d924d1282ee0a68eac322968c8f25d7d56
makki51x3 <96151955+makki51x3@users.noreply.github.com>
2022-02-03 07:49:47 -0800

    Add files via upload

--- code/code.ino
+++ code/code.ino
@@ -18,23 +18,43 @@ int y=0;
 
 void setup() {
 
-//  put your setup code here, to run once: pinMode(onpush,INPUT); pinMode(offpush,INPUT); pinMode(motor,OUTPUT); pinMode(in1,OUTPUT); pinMode(in2,OUTPUT); Serial.begin(9600);
+//  put your setup code here, to run once: 
+
+pinMode(onpush,INPUT); 
+pinMode(offpush,INPUT); 
+pinMode(motor,OUTPUT); 
+pinMode(in1,OUTPUT); 
+pinMode(in2,OUTPUT); 
+Serial.begin(9600);
 
 } 
 
 void loop() {
 
-//  put your main code here, to run repeatedly: onpushstate=digitalRead(onpush); offpushstate=digitalRead(offpush);
- if(onpushstate==HIGH && y==0){
-
-for( int i=0;i<=254;i++){ varout=varout+1; analogWrite(motor,varout); digitalWrite(in1,HIGH); digitalWrite(in2,LOW); delay(2000/255); Serial.println(varout);
-
-}
-y=1;
-}
-if(offpushstate==HIGH && y==1){
- for(int i=254;i>=0;i--){ varout=varout-1; analogWrite(motor,varout); digitalWrite(in1,HIGH); digitalWrite(in2,LOW); delay(2000/255); Serial.println(varout);
-}
-y=0;
-}
+  //  put your main code here, to run repeatedly:
+  onpushstate=digitalRead(onpush); 
+  offpushstate=digitalRead(offpush);
+  
+  if(onpushstate==HIGH && y==0){
+      for( int i=0;i<=254;i++){ 
+        varout=varout+1; 
+        analogWrite(motor,varout); 
+        digitalWrite(in1,HIGH); 
+        digitalWrite(in2,LOW); 
+        delay(2000/255); 
+        Serial.println(varout);
+      }
+      y=1;
+   }
+  if(offpushstate==HIGH && y==1){
+   for(int i=254;i>=0;i--){ 
+    varout=varout-1; 
+    analogWrite(motor,varout); 
+    digitalWrite(in1,HIGH); 
+    digitalWrite(in2,LOW); 
+    delay(2000/255); 
+    Serial.println(varout);
+    }
+    y=0;
+  }
 }
